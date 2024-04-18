dabf86c16bf8a8a41f9d49413822119b30f30f19
katechoi14 <kchoi2@stevens.edu>
2024-04-18 14:41:21 -0400

    updated
    added index.js file, along with modifying a few other files

---
+++ data/events.js
@@ -0,0 +1,12 @@
+import {events} from '../config/mongoCollections.js';
+import {ObjectId} from 'mongodb';
+import validation from '../validation.js';
+
+let exportedMethods = {
+
+
+
+
+}
+
+export default exportedMethods;
--- data/users.js
+++ data/users.js
@@ -1,8 +1,10 @@
 import {users} from '../config/mongoCollections.js';
 import {ObjectId} from 'mongodb'
+import validation from '../validation.js';
 
 
 let exportedMethods = {
+    
 
 }
 
