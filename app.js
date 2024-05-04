2869491a128e1ff5fb9106aa472d2c9fe5b2b237
lianna lariccia <liannalariccia@gmail.com>
2024-05-03 20:17:27 -0400

    Middleware and login/registration stuff

--- app.js
+++ app.js
@@ -36,6 +36,67 @@ app.set('view engine', 'handlebars');
 
 configRoutes(app);
 
+// 1
+app.use((req, res, next) => {
+  const isAuthenticated = req.session.user ? true : false;
+  const role = isAuthenticated ? req.session.user.role : 'Non-Authenticated User';
+  console.log(`[${new Date().toUTCString()}]: ${req.method} ${req.originalUrl} (${role})`);
+  if (req.originalUrl === '/') {
+      if (!isAuthenticated) {
+          return res.redirect('/login');
+      } 
+  }
+  next();
+});
+
+// Login middleware
+app.get('/login', (req, res, next) => {
+  console.log('in login middleware')
+  if (req.session.user) {
+    return res.redirect('/user');
+  }
+  next(); 
+}, (req, res) => {
+      return res.render('login');
+});
+
+// Register middleware
+app.get('/register', (req, res, next) => {
+  if (req.session.user) {
+      return res.redirect('/user');
+  }
+  next();
+}, (req, res) => {
+   return res.render('register');
+});
+
+// User profile middleware
+app.get('/user', (req, res, next) => {
+  if (!req.session.user) {
+      return res.redirect('/login');
+  }
+  next();
+}, (req, res) => {
+  const user = req.session.user
+  return res.render('user', {user: req.session.user, currentTime: req.session.currentTime});
+});
+
+//Logoout middleware
+app.get('/logout', (req, res, next) => {
+  if (!req.session.user) {
+      return res.redirect('/login');
+  }
+  next();
+}, (req, res) => {
+  req.session.destroy((err) => {
+      if (err) {
+          return res.status(500).send('Failed to log out');
+      }
+      return res.redirect('/login');
+  });
+});
+
+
 app.listen(3000, () => {
     console.log('Routes are running on http://localhost:3000');
 })
--- data/users.js
+++ data/users.js
@@ -2,71 +2,6 @@ import {users} from '../config/mongoCollections.js';
 import {ObjectId} from 'mongodb'
 import validation from '../validation.js';
 
-<<<<<<< HEAD
-export const getAllUsers = async () => {
-    const userCollection = await users();
-    const userList = await userCollection.find({}).toArray();
-    return userList;
-=======
-export const registerUser = async (
-    username,
-    email,
-    password,
-    role
-  ) => {
-    let userCollection = undefined;
-    try{
-      userCollection = await users();
-      const existingUser = await userCollection.findOne({ username: username });
-      if (existingUser) {
-        console.log('existing user')
-          throw new Error('There is already an existing user with that username.');
-      }
-      email = validateEmail(email);
-      username = validation.validateUsername(username);
-      password = validation.validatePassword(password)
-      role = validation.validateField(role, 4, 5, /^(admin|user)$/i, "Role must be 'admin' or 'user'.");
-    }catch(e){
-      throw new Error(e)
-    }
-    const saltRounds = 16;
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
-    const insertResult = await userCollection.insertOne({
-        email: email,
-        username: username,
-        password: hashedPassword,
-        role: role
-    });
-    if (insertResult.insertedCount === 0) {
-        throw new Error('User registration failed');
-    }
-    return { signupCompleted: true };
-  };
-
-  export const loginUser = async (username, password) => {
-    let user = undefined
-    try{
-      username = validateUsername(username);
-      password = validatePassword(password);
-  
-      const userCollection = await users();
-      user = await userCollection.findOne({username: username});
-      const isMatch = await bcrypt.compare(password, user.password);
-      if(!isMatch){
-      throw new Error('Either the username or password is invalid')
-      }
-      return {
-        email: user.email,
-        username: user.username,
-        role: user.role
-      };
-  
-    }catch(e){
-      throw new Error(e)
-    }
->>>>>>> a30860b (minor fixes)
-}
-
 //TODO: Add object id stuff
 export const registerUser = async (
     username,
@@ -85,7 +20,6 @@ export const registerUser = async (
       email = validateEmail(email);
       username = validation.validateUsername(username);
       password = validation.validatePassword(password)
-      role = validation.validateField(role, 4, 5, /^(admin|user)$/i, "Role must be 'admin' or 'user'.");
     }catch(e){
       throw new Error(e)
     }
