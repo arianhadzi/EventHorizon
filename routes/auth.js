37fb6884ea78f4697d43c3bb9e314b978d59915e
Hemanthbathini <167234010+Hemanthbathini@users.noreply.github.com>
2024-05-06 22:39:22 -0400

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,196 +1,185 @@
-import {Router} from 'express';
+import { Router } from "express";
 const router = Router();
-import * as users from '../data/users.js';
-import validation from '../validation.js';
-console.log(validation);
-import { ObjectId } from 'mongodb';
-import xss from 'xss';
+import * as users from "../data/users.js";
+import { validateUsername, validatePassword } from "../validation.js";
+import { ObjectId } from "mongodb";
+import xss from "xss";
 
+router.get("/", (req, res) => {
+  return res.render("home");
+});
 
-router.get('/', (req, res) => {
-    return res.render('home');
-  });
-
-router.get('/home', (req, res) => {
-  return res.render('home');
+router.get("/home", (req, res) => {
+  return res.render("home");
 });
 
 router
-  .route('/register')
+  .route("/register")
   .get(async (req, res) => {
-    res.render('register', { title: 'Register' });
+    res.render("register", { title: "Register" });
   })
   .post(async (req, res) => {
     // checking if firstname is supplied
+
     if (!req.body.firstName) {
-      return res.status(400).render('register', {
-        title: 'Register',
-        error: 'Error: firstName is missing',
+      return res.status(400).render("register", {
+        title: "Register",
+        error: "Error: firstName is missing",
       });
     }
     // checking if lastname is supplied
     if (!req.body.lastName) {
-      return res.status(400).render('register', {
-        title: 'Register',
-        error: 'Error: lastName is missing',
+      return res.status(400).render("register", {
+        title: "Register",
+        error: "Error: lastName is missing",
       });
     }
     // checking if username is supplied
     if (!req.body.username) {
-      return res.status(400).render('register', {
-        title: 'Register', 
-        error: 'Error: username is missing',
+      return res.status(400).render("register", {
+        title: "Register",
+        error: "Error: username is missing",
       });
     }
-    // checking if email is given 
+    // checking if email is given
     if (!req.body.email) {
-      return res.status(400).render('register', {
-        title: 'Register',
-        error: 'Error: email is missing',
+      return res.status(400).render("register", {
+        title: "Register",
+        error: "Error: email is missing",
       });
     }
     // checking if password is supplied
     if (!req.body.password) {
-      return res.status(400).render('register', {
-        title: 'Register',
-        error: 'Error: password is missing',
+      return res.status(400).render("register", {
+        title: "Register",
+        error: "Error: password is missing",
       });
     }
 
     let username = xss(req.body.username);
     let password = xss(req.body.password);
     // input validation
-    username = validation.validateUsername(username);
-    password = validation.validatePassword(password);
+    username = validateUsername(username);
+    password = validatePassword(password);
 
     try {
       let newUser = await users.registerUser(
         req.body.firstName,
         req.body.lastName,
         req.body.username,
-        req.body.password,
+        req.body.password
       );
       if (newUser.signupCompleted) {
-        return res.redirect('/login');
+        return res.redirect("/login");
       } else {
-        return res.status(500).render('register', {
-          title: 'Register',
-          error: 'Error: Internal Server Error',
+        return res.status(500).render("register", {
+          title: "Register",
+          error: "Error: Internal Server Error",
         });
       }
     } catch (e) {
-      return res.status(400).render('register', {
-        title: 'Register',
+      return res.status(400).render("register", {
+        title: "Register",
         error: e,
       });
     }
   });
 
 router
-  .route('/login')
+  .route("/login")
   .get(async (req, res) => {
-    return res.render('login', { title: 'Login'});
+    return res.render("login", { title: "Login" });
   })
   .post(async (req, res) => {
     if (!req.body.username) {
-      return res.status(400).render('login', {
-        title: 'Login',
-        error: 'Error: username should be supplied.',
+      return res.status(400).render("login", {
+        title: "Login",
+        error: "Error: username should be supplied.",
       });
     }
     if (!req.body.password) {
-      return res.status(400).render('login', {
-        title: 'Login',
-        error: 'Error: password should be supplied.',
+      return res.status(400).render("login", {
+        title: "Login",
+        error: "Error: password should be supplied.",
       });
     }
     let username = xss(req.body.username);
     let password = xss(req.body.password);
-    username = validation.validateUsername(username);
-    password = validation.validatePassword(password);
+    username = validateUsername(username);
+    password = validatePassword(password);
 
     try {
       let login = await users.loginUser(username, password);
-        req.session.user = login;
-        req.session.loggedIn = true;
-      } catch (e) {
-        return res.status(400).render('login', {
-          title: 'Login',
-          error: e,
-        });
-      }
+      req.session.user = login;
+      req.session.loggedIn = true;
+    } catch (e) {
+      return res.status(400).render("login", {
+        title: "Login",
+        error: e,
+      });
+    }
   });
 
-router
-  .route('/logout')
-  .get(async (req, res) => {
-    res.render('logout')
-})
+router.route("/logout").get(async (req, res) => {
+  res.render("logout");
+});
 
-router
-  .route('/user')
-  .get(async (req, res) => {
-    try{
-    if(req.session.user){
-      return res.render('user', {
+router.route("/user").get(async (req, res) => {
+  try {
+    if (req.session.user) {
+      return res.render("user", {
         firstName: req.session.firstName,
         lastName: req.session.user.lastName,
         currentTime: new Date().toLocaleTimeString(),
       });
     }
+  } catch (e) {
+    res.render("login");
   }
-  catch(e){
-    res.render('login');
-  }
-  });
-
-
-router
-  .route('/logout')
-  .get(async (req, res) => {
-    req.session.destroy();
-    res.render('logout', { title: 'Logout' });
-  });
-router.route('/create-event').get(async(req, res) => {
-  res.render('create_event')
 });
 
-router.route('/verify-organizer').get(async(req, res) => {
-  res.render('verify_organizer', {
-  })
+router.route("/logout").get(async (req, res) => {
+  req.session.destroy();
+  res.render("logout", { title: "Logout" });
 });
-
-router.route('/search').get(async(req, res) => {
-  res.render('search', {
-    
-  })
-});
-
-router.route('/calendar').get(async(req, res) => {
-  res.render('calendar', {
-    
-  })
-});
-
-router.route('/bookmarks').get(async(req, res) => {
-  res.render('bookmarks', {
-    
-  })
-});
-
-router.get('/home', (req, res) => {
-  res.render('home', {loggedIn: req.session.loggedIn});
-});
-
-router.get('/event', (req, res) => {
-  res.render('event', {
-  
+router.route("/create-event").get(async (req, res) => {
+  res.render("create_event", {
+    title: req.session.eventName,
+    description: req.session.eventDescription,
+    date: req.session.event.eventDate,
+    location: req.session.eventLocation,
+    eventId: new ObjectId(),
   });
 });
 
-router.get('/', (req, res) => {
-  res.render('home', {loggedIn: req.session.loggedIn});
+router.route("/verify-organizer").get(async (req, res) => {
+  res.render("verify_organizer", {
+    // organizationName: req.session.organizationName,
+  });
 });
-  
+
+router.route("/search").get(async (req, res) => {
+  res.render("search", {});
+});
+
+router.route("/calendar").get(async (req, res) => {
+  res.render("calendar", {});
+});
+
+router.route("/bookmarks").get(async (req, res) => {
+  res.render("bookmarks", {});
+});
+
+router.get("/home", (req, res) => {
+  res.render("home", req.session.loggedIn);
+});
+
+router.get("/event", (req, res) => {
+  res.render("event", {});
+});
+
+router.get("/", (req, res) => {
+  res.render("home", req.session.loggedIn);
+});
 
 export default router;
