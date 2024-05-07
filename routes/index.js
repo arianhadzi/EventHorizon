4867b70a440606ea087ee73d53ef625b23e83a63
Hemanthbathini <167234010+Hemanthbathini@users.noreply.github.com>
2024-05-06 22:38:25 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,11 @@
-import authRoutes from './auth.js';
+import authRoutes from "./auth.js";
 
 const constructorMethod = (app) => {
-  app.use('/', authRoutes);
+  app.use("/", authRoutes);
 
-  app.use('*', (req, res) => {
-    res.status(404).send('Not Found'); 
+  app.use("*", (req, res) => {
+    res.status(404).json({ error: "Route Not found" });
   });
-  
 };
 
 export default constructorMethod;
