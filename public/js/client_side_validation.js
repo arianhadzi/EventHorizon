66a312c484e65f80470e67995610b1f69d742d5d
katechoi14 <kchoi2@stevens.edu>
2024-05-06 21:57:40 -0400

    updated client_side_validation

--- public/js/client_side_validation.js
+++ public/js/client_side_validation.js
@@ -9,19 +9,39 @@ let email = document.getElementById('email');
 let username = document.getElementById('username');
 let password = document.getElementById('password');
 let confirmPassword = document.getElementById('confirmPassword');
-let registerError = document.getElementById('signup-error-form');
-let loginError = document.getElementById('signin-error-form');
 
 if (registerForm) {
     registerForm.addEventListener('submit', (event) => {
         event.preventDefault();
         let errorMessages = [];
-        registerError.innerHTML = '';
-        registerError.hidden = true;
 
-        if (!validation.validateName(firstName)) {
-            errorMessages.push('First name needs value.');
+        if (!firstName.value) {
+            errorMessages.push('First name is missing!');
         }
+        if (!lastName.value) {
+            errorMessages.push('Last name is missing!');
+        }
+        if (!email.value) {
+            errorMessages.push('Email is missing!');
+        }
+        if (!username.value) {
+            errorMessages.push('Username is missing!');
+        }
+        if (!password.value) {
+            errorMessages.push('Password is missing!');
+        }
+        if (!confirmPassword.value) {
+            errorMessages.push('ConfirmPassword is missing!');
+        }
+
+        firstName.value = firstName.value.trim();
+        lastName.value = lastName.value.trim();
+        email.value = email.value.trim();
+        username.value = username.value.trim();
+        password = password.value.trim();
+        confirmPassword = confirmPassword.value.trim();
+
+        
 
     })
 }
